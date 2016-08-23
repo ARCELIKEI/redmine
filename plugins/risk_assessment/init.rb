c5a9d9e5165b420ff5c8a480994542237f21b7bc
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-08-23 14:09:51 +0300

    permission bug of risk assessment plugin fixed.

--- plugins/risk_assessment/init.rb
+++ plugins/risk_assessment/init.rb
@@ -2,15 +2,12 @@ Redmine::Plugin.register :risk_assessment do
   name 'Risk Assessment plugin'
   author 'Arcelik AR-GE'
   description 'Risk assessment for projects. Need to be selected from project settings.'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/ARCELIKEI/redmine/tree/arriva-3.0/plugins/risk_assessment'
 
 
   add_tab :critical_value, :partial => 'tab/critical_value_set'
   tab_settings :default => {'low_to_mid_threshold' => '2','mid_to_high_threshold' => '5' }
-
- 	permission :risk_statuses, { :risk_statuses => [:index] }, :public => true
-  permission :risks, { :risks => [:index] }, :public => true
 
   menu :admin_menu, :risk_assessment, {:controller => 'risk_statuses', :action => 'index'},
 		:caption => :field_risk_statuses
@@ -19,7 +16,7 @@ Redmine::Plugin.register :risk_assessment do
        	:caption => :field_risk_assessment, :param => :project_id
 
   project_module :risk_assessment do
-    permission :risk_assessment, {:risk_assessment => [:index]}, :require => :member
+    permission :view_risks, :risks => :index, :require => :member
   end
 
 end
